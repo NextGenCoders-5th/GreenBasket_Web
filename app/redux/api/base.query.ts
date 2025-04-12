cf5300c120983e3213fbacec341f54a4240332d0
eziraa <ezratgab@gmail.com>
2025-04-12 18:50:45 +0300

    feat: create base query to refresh token

--- app/redux/api/base.query.ts
+++ app/redux/api/base.query.ts
@@ -2,15 +2,9 @@ import { BaseQueryApi, FetchArgs, fetchBaseQuery } from '@reduxjs/toolkit/query/
 import { toast } from 'sonner';
 
 const BASE_URL = process.env.NEXT_APP_API_URL;
-const baseQuery = fetchBaseQuery({
+export const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
-  prepareHeaders: (headers, { getState }) => {
-    const token = localStorage.getItem(LocalStorage.TOKEN);
-    if (token) {
-      headers.set('Authorization', `Bearer ${token}`);
-    }
-    return headers;
-  },
+  credentials: 'include',
 });
 
 export const baseQueryWithReauth = async (args: string | FetchArgs, api: BaseQueryApi, extraOptions: {}) => {
@@ -18,33 +12,20 @@ export const baseQueryWithReauth = async (args: string | FetchArgs, api: BaseQue
   console.log(result)
 
   if (result.error && result.error.status === 401) {
-    console.log(result.error)
-    const refreshToken = localStorage.getItem(LocalStorage.REFRESH_TOKEN);
-
-    if (!refreshToken) {
-      toast.error("No refresh token available, logging out...");
-      api.dispatch(logout());
-      return result;
-    }
-
+    
     // Attempt token refresh
     const refreshResult = await baseQuery(
       {
-        url: 'Auth/RefreshToken', // Your refresh token endpoint
+        url: 'auth/refresh', // Your refresh token endpoint
         method: 'POST',
-        body: { 
-          RefreshToken: localStorage.getItem(LocalStorage.REFRESH_TOKEN),
-          Token: localStorage.getItem(LocalStorage.TOKEN),
-         },
+        
       },
       api,
       extraOptions
     );
 
     if (refreshResult.data) {
-      console.log("Token refresh successful, retrying request...");
       const refreshedData = refreshResult.data as { token: string; refreshToken: string }; // Explicitly type the response
-      api.dispatch(setCredentials(refreshedData));
 
       localStorage.setItem("token", refreshedData.token);
       localStorage.setItem("refreshToken", refreshedData.refreshToken);
@@ -52,7 +33,7 @@ export const baseQueryWithReauth = async (args: string | FetchArgs, api: BaseQue
       result = await baseQuery(args, api, extraOptions);
     } else {
       console.log("Token refresh failed, logging out...");
-      api.dispatch(logout());
+      // api.dispatch(logout());
     }
   }
   else if (result.error?.status === "FETCH_ERROR") {
