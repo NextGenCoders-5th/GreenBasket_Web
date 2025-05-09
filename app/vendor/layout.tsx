4b151f5f1b3877f254692f510e1f0531f51a5711
eziraa <ezratgab@gmail.com>
2025-05-09 12:43:36 +0300

    feat: vendor dashboard

---
+++ app/vendor/layout.tsx
@@ -0,0 +1,37 @@
+import { AppSidebar } from '@/components/app-sidebar';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import { Role } from '@/enums/role.enum';
+import PermissionProvider from '@/providers/permission.provider';
+import ProtectedProvider from '@/providers/protected.provider';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+  description: 'Dashboard',
+  icons: {
+    icon: '/logo.png',
+  },
+};
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
+  return (
+    <ProtectedProvider>
+      <PermissionProvider roles={[Role.VENDOR]}>
+        <SidebarProvider
+          style={
+            {
+              '--sidebar-width': 'calc(var(--spacing) * 72)',
+              '--header-height': 'calc(var(--spacing) * 12)',
+            } as React.CSSProperties
+          }
+        >
+          <AppSidebar variant="inset" />
+          <SidebarInset>{children}</SidebarInset>
+        </SidebarProvider>
+      </PermissionProvider>
+    </ProtectedProvider>
+  );
+}
