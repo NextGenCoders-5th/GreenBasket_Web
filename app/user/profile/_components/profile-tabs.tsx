4487c68c849f6e689a5c218909ade5ee83327605
eziraa <ezratgab@gmail.com>
2025-05-28 09:31:10 +0300

    update: profile tab to display necessary infomation based on user role

--- app/user/profile/_components/profile-tabs.tsx
+++ app/user/profile/_components/profile-tabs.tsx
@@ -1,10 +1,13 @@
 "use client"
-import { UserIcon, PencilIcon, Plus, MapPinHouse} from "lucide-react"
+import { Role } from "@/enums/role.enum"
+import { useAppSelector } from "@/redux/store"
+import { IUser } from "@/types/user.type"
+import { UserIcon, PencilIcon, Plus, MapPinHouse } from "lucide-react"
 
 
-export enum TabsValueEnum{
-  ABOUT ="about",
-  EDIT  = "edit",
+export enum TabsValueEnum {
+  ABOUT = "about",
+  EDIT = "edit",
   CHANGE_PASSWORD = "changepassword",
   ADD_ADDRESS = "add_address"
 }
@@ -14,12 +17,14 @@ interface ProfileTabsProps {
 }
 
 export default function ProfileTabs({ activeTab, setActiveTab }: ProfileTabsProps) {
+  const user = useAppSelector((state => state.auth.user)) as IUser | null;
   const tabs = [
     { id: TabsValueEnum.ABOUT, label: "About", icon: UserIcon },
     { id: TabsValueEnum.EDIT, label: "Change Profile Picture", icon: PencilIcon },
-    {id: TabsValueEnum.ADD_ADDRESS, label: "My Address", icon:MapPinHouse },
+    { id: TabsValueEnum.ADD_ADDRESS, label: "My Address", icon: MapPinHouse },
     { id: TabsValueEnum.CHANGE_PASSWORD, label: "Change Password", icon: PencilIcon }
   ]
+    .filter((tab) => tab.id !== TabsValueEnum.ADD_ADDRESS || user?.role === Role.CUSTOMER);
 
   return (
     <div className="flex gap-3 p-6 pt-2 w-full  ">
@@ -27,14 +32,13 @@ export default function ProfileTabs({ activeTab, setActiveTab }: ProfileTabsProp
         <button
           key={tab.id}
           onClick={() => setActiveTab(tab.id)}
-          className={`flex items-center gap-2 px-4 py-2 rounded-md text-sm ${
-            activeTab === tab.id
+          className={`flex items-center gap-2 px-4 py-2 rounded-md text-sm ${activeTab === tab.id
               ? "bg-green-600/90 text-white"
               : "dark:bg-gray-700 bg-gray-200 dark:text-gray-300 text-gray-700"
-          }`}
+            }`}
         >
           <tab.icon className="w-4 h-4" />
-        <span className="hidden md:inline">  {tab.label.split(" ")[0]}</span> {tab.label.split(" ")[1]}
+          <span className="hidden md:inline">  {tab.label.split(" ")[0]}</span> {tab.label.split(" ")[1]}
         </button>
       ))}
     </div>
